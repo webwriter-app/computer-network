7a2b1e8147e9d84d9b9657cd8db9a8343adda3e3
robjonkre <robin.jonas.kreft@gmail.com>
2024-09-04 15:00:18 +0200

    updated display property for options

--- src/index.ts
+++ src/index.ts
@@ -526,7 +526,7 @@ export class NetworkComponent extends LitElementWw {
 
     private asideTemplate(): TemplateResult {
         return html`
-            <aside part="options" style="visibility: hidden">
+            <aside part="options" style="display: none">
                 <form autocomplete="off">
                     <input class="importBtn" style="width: 11cqw;" type="file" id="import-file" />
                     <sl-tooltip content="Import a file created by this widget" placement="bottom">
